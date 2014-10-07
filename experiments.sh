1695ec8c18b7f4cbe3abecf7992f96acbc4e6ff8
Sergey Mechtaev <mechtaev@gmail.com>
2014-10-07 18:31:31 +0800

    multiple hosts

--- experiments.sh
+++ experiments.sh
@@ -4,8 +4,10 @@
 # then add your ~/.ssh/id_rsa.pub to ~/.ssh/authorized_keys for all remote nodes.
 
 USER="semfix"
-NODE=127.0.0.1
+NODES=( 127.0.0.1 )
 PORT=3022
+
+SESSION=seryozha-experiments
 
 SSH="ssh -q -p $PORT"
 SCP="scp -q -P $PORT"
@@ -17,53 +19,69 @@ CO='\e[0;34m'
 NC='\e[0m' # No Color
 
 start_session() {
-    $SSH $USER@$NODE "screen -d -m"
+    node=$1
+    $SSH $USER@$node "screen -dmS $SESSION"
 }
 
 stop_session() {
-    $SSH $USER@$NODE "screen -p 0 -X kill"
+    node=$1
+    $SSH $USER@$node "screen -S $SESSION -p 0 -X kill"
 }
 
 get_screen() {
-    tmp_remote=`$SSH $USER@$NODE mktemp`
+    node=$1
+    tmp_remote=`$SSH $USER@$node mktemp`
     tmp_local=`mktemp`
-    $SSH $USER@$NODE "screen -p 0 -X hardcopy $tmp_remote"
-    $SCP $USER@$NODE:$tmp_remote $tmp_local
+    $SSH $USER@$node "screen -S $SESSION -p 0 -X hardcopy $tmp_remote"
+    $SCP $USER@$node:$tmp_remote $tmp_local
     cat $tmp_local
 }
 
 exec_remote() {
-    cmd="$1; if [[ \\\$? -eq 0 ]]; then touch ~/$SUCCEEDED; else touch ~/$FAILED; fi"
-    $SSH $USER@$NODE "rm ~/$SUCCEEDED ~/$FAILED 2> /dev/null; screen -p 0 -X stuff \"$cmd\"; screen -p 0 -X eval 'stuff \\015'"
+    node=$1
+    cmd="$2; if [[ \\\$? -eq 0 ]]; then touch ~/$SUCCEEDED; else touch ~/$FAILED; fi"
+    $SSH $USER@$node "rm ~/$SUCCEEDED ~/$FAILED 2> /dev/null; screen -S $SESSION -p 0 -X stuff \"$cmd\"; screen -S $SESSION -p 0 -X eval 'stuff \\015'"
 }
 
 check_status() {
-    printf "Node: %s\n" $NODE 
-    status=$($SSH $USER@$NODE "[[ -f ~/$SUCCEEDED ]] && echo SUCCESS; [[ -f ~/$FAILED ]] && echo FAIL")
+    node=$1
+    printf "Node: %s\n" $node 
+    status=$($SSH $USER@$node "[[ -f ~/$SUCCEEDED ]] && echo SUCCESS; [[ -f ~/$FAILED ]] && echo FAIL")
     if [[ -n "$status" ]]; then
         printf "%s\n" "Status: $status"
     else
         printf "%s\n" "Status: IN PROGRESS"
     fi
-    printf "${CO}" ""; get_screen; printf "${NC}" ""
+    printf "${CO}" ""; get_screen $node; printf "${NC}" ""
 }
 
 
 case "$1" in
     start)
-        start_session
+        for node in "${NODES[@]}"; do
+            start_session $node
+        done
         ;;
     stop)
-        stop_session
+        for node in "${NODES[@]}"; do
+            stop_session $node
+        done
         ;;
     exec)
-        exec_remote "$2"
+        for node in "${NODES[@]}"; do
+            exec_remote $node "$2"
+        done
         ;;
     submit)
-        submit_task "$2"
+        echo "not implemented"
+        # for node in "${NODES[@]}"; do
+        #     submit_task $node "$2"
+        # done
         ;;
     status)
-        check_status
+        for node in "${NODES[@]}"; do
+            check_status $node
+        done
         ;;
     *)
         echo "Usage: `basename $0` { start | stop | exec CMD | submit TASK | status }"
